6a2838ddc2d2e8c45100d1c8b3940fe9be065fb8
H-Abe0904 <113865937+H-Abe0904@users.noreply.github.com>
2025-12-10 09:41:41 +0900

    aa

--- obj/Debug/net10.0/stockDataImporter.AssemblyInfo.cs
+++ obj/Debug/net10.0/stockDataImporter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("stockDataImporter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc3d307e4cdc133f7fb86c2ee5064021a0abc82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb16a60f63c8557c71b762fab2432b6ff3f4916a")]
 [assembly: System.Reflection.AssemblyProductAttribute("stockDataImporter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("stockDataImporter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
